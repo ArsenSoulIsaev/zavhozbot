18d405360c0dc52bfdf39d714a61579666927004
ArsenSoulIsaev <arsen.soul.isaev@gmail.com>
2026-03-16 23:41:24 +0300

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,24 +1,5 @@
 export type UserRole = "arsen" | "prorab" | "employee";
 
-export interface TelegramMessage {
-  update_id: number;
-  message?: {
-    message_id: number;
-    date: number;
-    text?: string;
-    chat: {
-      id: number;
-      type: string;
-    };
-    from?: {
-      id: number;
-      is_bot: boolean;
-      first_name?: string;
-      username?: string;
-    };
-  };
-}
-
 export interface AuthUser {
   id: number;
   telegram_id: number | null;
