a9a7def394393e0d134bb8559e77980cbf13aada
ArsenSoulIsaev <arsen.soul.isaev@gmail.com>
2026-03-16 12:16:06 +0300

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -2,11 +2,15 @@ import pg from "pg";
 import { config } from "./config.js";
 
 const { Pool } = pg;
+type QueryResultRow = pg.QueryResultRow;
 
 export const pool = new Pool({
   connectionString: config.databaseUrl
 });
 
-export async function query<T = unknown>(text: string, params: unknown[] = []) {
+export async function query<T extends QueryResultRow = QueryResultRow>(
+  text: string,
+  params: unknown[] = []
+) {
   return pool.query<T>(text, params);
 }
